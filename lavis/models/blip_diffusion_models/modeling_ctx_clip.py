bd8180213a949de90f2ae7189d00f31f679c7c2d
TuanDung111 <tuandung12092002@gmail.com>
2025-01-02 09:42:44 +0000

    update modeling_ctx_clip.py

--- lavis/models/blip_diffusion_models/modeling_ctx_clip.py
+++ lavis/models/blip_diffusion_models/modeling_ctx_clip.py
@@ -4,6 +4,7 @@
  SPDX-License-Identifier: BSD-3-Clause
  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 """
+
 from typing import Optional, Tuple, Union
 
 import torch
@@ -13,8 +14,18 @@ from transformers.models.clip.configuration_clip import CLIPTextConfig
 from transformers.models.clip.modeling_clip import (
     CLIPEncoder,
     CLIPPreTrainedModel,
-    _expand_mask,
+    # _expand_mask,
 )
+
+
+def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+    bsz, src_len = mask.size()
+    tgt_len = tgt_len if tgt_len is not None else src_len
+    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+    inverted_mask = 1.0 - expanded_mask
+    return inverted_mask.masked_fill(
+        inverted_mask.to(torch.bool), torch.finfo(dtype).min
+    )
 
 
 class CtxCLIPTextModel(CLIPPreTrainedModel):
